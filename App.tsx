bbb8bfa3ef7af425d4ef176b489ac5c7d8cc70ac
André Varandas <andre.m.varandas@gmail.com>
2022-10-24 00:24:41 +0100

    :construction: (navigation) adds navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useFonts } from 'expo-font'
 import { locale } from 'expo-localization'
 import { StatusBar } from 'expo-status-bar'
 import { I18n } from 'i18n-js'
+import { ActivityIndicator } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import en from './i18n/en.json'
 import pt from './i18n/pt.json'
@@ -13,7 +15,31 @@ import HomeScreen from './screens/HomeScreen'
 const translations = { en, pt }
 const i18n = new I18n(translations, { locale, enableFallback: true })
 
+const AppTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#fff',
+  },
+}
+
+const stackNavigatorOptions = {
+  headerShown: false,
+}
+
 const Stack = createNativeStackNavigator()
+
+const root = (
+  <SafeAreaProvider>
+    <NavigationContainer theme={AppTheme}>
+      <Stack.Navigator screenOptions={stackNavigatorOptions}>
+        <Stack.Screen name="Register" component={HomeScreen} />
+        <Stack.Screen name="Chests" component={ChestsScreen} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  </SafeAreaProvider>
+)
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,13 +47,5 @@ export default function App() {
     'supercell-magic': require('./assets/fonts/supercell-magic.ttf'),
   })
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Register" component={HomeScreen} />
-        <Stack.Screen name="Chests" component={ChestsScreen} />
-        <StatusBar style="auto" />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+  return fontsLoaded ? root : <ActivityIndicator size="large" />
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,12 @@
 import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
-  return <Text>Hello from the home screen!</Text>
+  return (
+    <SafeAreaView
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Text>Hello from the home screen!</Text>
+    </SafeAreaView>
+  )
 }
