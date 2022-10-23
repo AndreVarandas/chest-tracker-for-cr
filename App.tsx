3e5e06fb8e66a4bad890525dc52e63fa056d3aad
André <andre.m.varandas@gmail.com>
2022-10-23 01:28:56 +0100

    :globe_with_meridians: (i18n) adds i18n support (#17)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
+import { locale } from 'expo-localization'
 import { StatusBar } from 'expo-status-bar'
+import { I18n } from 'i18n-js'
 import { StyleSheet, Text, View } from 'react-native'
+
+import en from './i18n/en.json'
+import pt from './i18n/pt.json'
+
+const translations = { en, pt }
+const i18n = new I18n(translations, { locale, enableFallback: true })
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{i18n.t('register.intro')}</Text>
       <StatusBar style="auto" />
     </View>
   )
