2fb8dc75d24ca0e5e5e830b4325af50ce5707b93
André <andre.m.varandas@gmail.com>
2022-10-24 21:46:37 +0100

    Feat/add navigation (#20)
    * :construction: (navigation) adds navigation
    
    * :construction: (navigation) adds navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,45 @@
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useFonts } from 'expo-font'
 import { locale } from 'expo-localization'
 import { StatusBar } from 'expo-status-bar'
 import { I18n } from 'i18n-js'
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import en from './i18n/en.json'
 import pt from './i18n/pt.json'
+import ChestsScreen from './screens/ChestsScreen'
+import HomeScreen from './screens/HomeScreen'
 
 const translations = { en, pt }
 const i18n = new I18n(translations, { locale, enableFallback: true })
+
+const AppTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#fff',
+  },
+}
+
+const stackNavigatorOptions = {
+  headerShown: false,
+}
+
+const Stack = createNativeStackNavigator()
+
+const root = (
+  <SafeAreaProvider>
+    <NavigationContainer theme={AppTheme}>
+      <Stack.Navigator screenOptions={stackNavigatorOptions}>
+        <Stack.Screen name="Register" component={HomeScreen} />
+        <Stack.Screen name="Chests" component={ChestsScreen} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  </SafeAreaProvider>
+)
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,19 +47,5 @@ export default function App() {
     'supercell-magic': require('./assets/fonts/supercell-magic.ttf'),
   })
 
-  return (
-    <View style={styles.container}>
-      <Text>{i18n.t('register.intro')}</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+  return fontsLoaded ? root : <ActivityIndicator size="large" />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
